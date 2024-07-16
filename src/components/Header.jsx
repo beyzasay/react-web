71bc8dbfe4ab3220f8d0aacefca1ea96ccb945f7
Beyza Say <beyzasayy@gmail.com>
2024-07-16 20:18:01 +0300

    .

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import '../css/Header.css';
+
 
 function Header() {
     return (
-        <div>Header</div>
+        <div style={{ display: 'flex', flexDiection: 'row', alignItems: 'center', justifyContent: 'space-between' }}>
+            <div className='flex-row'>
+                <img className='logo' src="./src/images/logo.png.png" />
+                <p className='logo-text'> badex </p>
+            </div>
+
+            <div>
+                <inpıt className="search-input" type="text" />
+            </div>
+        </div>
     )
 }
 
