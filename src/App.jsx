b9524220128df5e955cfc11c06774d24751608cd
Beyza Say <beyzasayy@gmail.com>
2024-07-17 14:45:22 +0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,16 @@ import React from 'react'
 import './App.css'
 import PageContainer from './container/PageContainer'
 import Header from './components/Header'
+import ProductList from './components/ProductList'
 
 
 function App() {
 
 
   return (
-    <div id="root"> <PageContainer>
+    <div > <PageContainer>
       <Header />
+      <ProductList />
     </PageContainer>
     </div>
   )
---
+++ src/redux/store.jsx
@@ -0,0 +1,12 @@
+import { configureStore } from '@reduxjs/toolkit'
+import appReducer from '../redux/slices/appSlice';
+import ProductReducer from '../redux/slices/productSlice';
+
+
+
+export const store = configureStore({
+    reducer: {
+        app: appReducer,
+        product: productsReducer
+    },
+})
