1500b09b53202ba309a32a67d781e2fbf071407c
Beyza Say <beyzasayy@gmail.com>
2024-07-18 21:16:01 +0300

    .

--- src/components/ProductDetails.jsx
+++ src/components/ProductDetails.jsx
@@ -5,15 +5,16 @@ import { setSelectedProduct } from '../redux/slices/productSlice';
 
 function ProductDetails() {
   const { id } = useParams();
-  const { products, selectedProduct } = useSelector((store) => store.prodcut)
+  const { products, selectedProduct } = useSelector((store) => store.product)
+
+  const { price, image, title, description } = selectedProduct;
 
   const dispatch = useDispatch();
 
   useEffect(() => {
     getProductById();
 
-
-  }, [])
+  })
 
   const getProductById = () => {
     products && products.map((product) => {
@@ -24,7 +25,17 @@ function ProductDetails() {
     })
   }
   return (
-    <div>{selectedProduct.title} </div>
+    <div style={{ marginTop: '40px', display: 'flex', flexDirection: 'row', justifyContent: 'center' }}>
+      <div style={{ marginRight: '10px' }}>
+        <img src={image} width={300} height={450} alt="" />
+      </div>
+      <div>
+        <h1 style={{ fontFamily: 'arial' }}> {title} </h1>
+        <p style={{ fontFamily: 'arial', fontSize: '20px' }} > {description} </p>
+        <h1 style={{ fontSize: '50px', fontFamily: 'arial', fontWeight: 'bold', color: 'black' }}> {price}</h1>
+      </div>
+
+    </div>
   )
 }
 
